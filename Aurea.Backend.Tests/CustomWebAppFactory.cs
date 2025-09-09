f1ba4a3464945c63ffec210afcd6f58461fd11d3
avainfo <avainfoconseils@gmail.com>
2025-09-09 22:50:12 +0100

    test(smoke): add basic integration tests
    - Introduced CustomWebAppFactory for integration testing
    - Added SmokeTests covering GET / and GET /health endpoints
    - Removed placeholder UnitTest1

---
+++ Aurea.Backend.Tests/CustomWebAppFactory.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Aurea.Backend.Tests;
+
+public class CustomWebAppFactory : WebApplicationFactory<Program>
+{
+}
--- Aurea.Backend.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Aurea.Backend.Tests;
-
-public class UnitTest1
-{
-	[Fact]
-	public void Test1()
-	{
-	}
-}
